c6659c6069b1d649f137572bc4faffd00759de45
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2023-08-03 22:39:48 +0100

    create the column is_justifed in absence table and create the justfied absence table to insert the start date and end date of absence justfied with the stagaire id and make a view where disply the absence justfied

--- public/scripts/absences.js
+++ public/scripts/absences.js
@@ -45,6 +45,66 @@
 
 
 
+// <!-- //inscre les absence justifie and disply the model of them -->
+  // Event listener for the specific justified checkbox
+  const justifiedCheckboxes = document.querySelectorAll('[data-justified-checkbox]');
+  justifiedCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+      if (this.checked) {
+        // Show the modal when the checkbox is checked
+        const stagiaireId = this.getAttribute('data-stagiaire-id'); // Use data-stagiaire-id attribute instead of data-absence-id
+        showJustifiedAbsenceModal(stagiaireId); // Pass stagiaireId instead of absenceId
+      }
+    });
+  });
+
+  // Function to show the justified absence modal
+  function showJustifiedAbsenceModal(stagiaireId) { // Use stagiaireId instead of absenceId
+    document.getElementById('justifiedAbsenceModal').setAttribute('data-stagiaire-id', stagiaireId); // Use data-stagiaire-id attribute instead of data-absence-id
+    document.getElementById('startDate').value = '';
+    document.getElementById('endDate').value = '';
+    $('#justifiedAbsenceModal').modal('show');
+  }
+
+  // Function to handle the form submission and create the justified absence record
+  function saveJustifiedAbsence() {
+    const stagiaireId = document.getElementById('justifiedAbsenceModal').getAttribute('data-stagiaire-id'); // Use data-stagiaire-id attribute instead of data-absence-id
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+
+    // Perform any validation if needed (e.g., check if the dates are valid)
+
+    // Send the form data to the server using fetch or any other method
+    const formData = {
+      stagiaireId, // Use stagiaireId instead of absenceId
+      startDate,
+      endDate,
+    };
+
+    // Make a POST request to your server to create the justified absence
+    fetch('/justified-absences', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server (e.g., show a success message)
+        console.log(data);
+        // Optionally, you can hide the modal after creating the justified absence
+        $('#justifiedAbsenceModal').modal('hide');
+      })
+      .catch((error) => {
+        // Handle any error that occurred during the POST request
+        console.error('Error creating justified absence:', error);
+      });
+  }
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // Add event listeners to the attendance checkboxes
     const attendanceCheckboxes = document.querySelectorAll('.attendance-checkbox');
