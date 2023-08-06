815412d40eeef95e56c5d8a74ab7b4969ee5683b
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2023-08-06 18:44:32 +0100

    fix the fetching of all absence for each stg

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -8,7 +8,7 @@ pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
   try {
     const stagiaireId = parseInt(req.params.stagiaireId);
     const pdfFileName = await pdfController.generatePDF(stagiaireId);
-    res.redirect("/");
+    res.redirect("/absence/absences/function");
     // res.status(200).send(`PDF generated for Stagiaire ID ${stagiaireId}: ${pdfFileName}`);
   } catch (err) {
     res.status(500).send(`Error generating PDF: ${err.message}`);
