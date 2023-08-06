ea2c666f8dd33e056222aacda405976accbd0689
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2023-08-06 18:55:02 +0100

    make the generated pdf remove after download it

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -6,14 +6,30 @@ const pdfController = require("../controllers/pdfController");
 
 // Route to generate PDF for a specific stagiaire by ID
 // pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
-//   try {
-//     const stagiaireId = parseInt(req.params.stagiaireId);
-//     const pdfFileName = await pdfController.generatePDF(stagiaireId);
-//     res.redirect("/absence/absences/function");
-//   } catch (err) {
-//     res.status(500).send(`Error generating PDF: ${err.message}`);
-//   }
-// });
+//     try {
+//       const stagiaireId = parseInt(req.params.stagiaireId);
+//       const pdfFileName = await pdfController.generatePDF(stagiaireId);
+  
+//       // Wait for the PDF to be generated
+//       await new Promise((resolve) => setTimeout(resolve, 1000)); // Add a delay if needed
+  
+//       // Trigger the download
+//       res.download(pdfFileName, (err) => {
+//         if (err) {
+//           res.status(500).send(`Error downloading PDF: ${err.message}`);
+//         } else {
+//           // Delete the file after download (optional)
+//           fs.unlink(pdfFileName, (unlinkErr) => {
+//             if (unlinkErr) {
+//               console.error("Error deleting PDF file:", unlinkErr);
+//             }
+//           });
+//         }
+//       });
+//     } catch (err) {
+//       res.status(500).send(`Error generating PDF: ${err.message}`);
+//     }
+//   });
 pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
     try {
       const stagiaireId = parseInt(req.params.stagiaireId);
@@ -27,7 +43,7 @@ pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
         if (err) {
           res.status(500).send(`Error downloading PDF: ${err.message}`);
         } else {
-          // Delete the file after download (optional)
+          // Delete the file after download
           fs.unlink(pdfFileName, (unlinkErr) => {
             if (unlinkErr) {
               console.error("Error deleting PDF file:", unlinkErr);
