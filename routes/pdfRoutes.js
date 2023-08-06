d26b7a0817d4ba2db5b530af9a2b17719e4cdbde
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2023-08-06 18:52:51 +0100

    add the pdf to be download

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,18 +1,43 @@
 // routes/pdfRoutes.js
+const fs = require("fs");
 const express = require("express");
 const pdfRoutes = express.Router();
 const pdfController = require("../controllers/pdfController");
 
 // Route to generate PDF for a specific stagiaire by ID
+// pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
+//   try {
+//     const stagiaireId = parseInt(req.params.stagiaireId);
+//     const pdfFileName = await pdfController.generatePDF(stagiaireId);
+//     res.redirect("/absence/absences/function");
+//   } catch (err) {
+//     res.status(500).send(`Error generating PDF: ${err.message}`);
+//   }
+// });
 pdfRoutes.get("/pdf/:stagiaireId", async (req, res) => {
-  try {
-    const stagiaireId = parseInt(req.params.stagiaireId);
-    const pdfFileName = await pdfController.generatePDF(stagiaireId);
-    res.redirect("/absence/absences/function");
-    // res.status(200).send(`PDF generated for Stagiaire ID ${stagiaireId}: ${pdfFileName}`);
-  } catch (err) {
-    res.status(500).send(`Error generating PDF: ${err.message}`);
-  }
-});
+    try {
+      const stagiaireId = parseInt(req.params.stagiaireId);
+      const pdfFileName = await pdfController.generatePDF(stagiaireId);
+  
+      // Wait for the PDF to be generated
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Add a delay if needed
+  
+      // Trigger the download
+      res.download(pdfFileName, (err) => {
+        if (err) {
+          res.status(500).send(`Error downloading PDF: ${err.message}`);
+        } else {
+          // Delete the file after download (optional)
+          fs.unlink(pdfFileName, (unlinkErr) => {
+            if (unlinkErr) {
+              console.error("Error deleting PDF file:", unlinkErr);
+            }
+          });
+        }
+      });
+    } catch (err) {
+      res.status(500).send(`Error generating PDF: ${err.message}`);
+    }
+  });
 
 module.exports = pdfRoutes;
